9e7ca1700c5996ec0c235c9d8ed99cc396ae3e60
junay <jre200080@gmail.com>
2017-07-18 14:36:19 +0800

    完成form

--- 1.php
+++ 1.php
@@ -1,6 +1,25 @@
-i<?php
+<html>
+<head>
+    <h1>第一个PHP程序</h1>
+    <h2> <a href="http://baidu.com/">jump to baidu </a> </h2>
+</head>
 
-    echo "heloo git";
+<body>
+<form method="get" action="<?php echo $_SERVER["PHP_SELF"]?>">
+    账号<input type="text" name="username">
+    密码<input type="text" name="password">
+    提交<input type="submit">
+</form>
+
+<?php
+    echo "当前路径<br>";
+    echo $_SERVER["PHP_SELF"];
+    echo "<hr></hr>";
+    echo "<br>";
+    echo "hello git";
     for ($i=0; $i < 10; $i++) {
         echo "hello git <br>";
     }
+?>
+</body>
+</html>
