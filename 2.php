9c1e06551eaf5e8901cd80bd6ce37b345cda05c2
Junay <jay200080@163.com>
2017-07-18 22:16:07 +0800

    大佬完整的phpstrom项目

--- 2.php
+++ 2.php
@@ -1,8 +1,17 @@
-<?php
-	echo "大佬的第一份php";
-	echo "大佬要推到github";
-    echo "小弟热烈欢迎大佬加入";
-    echo "大佬万岁！！！";
-    echo "<br>嗯嗯，本大佬带你飞。";
+<html>
+<head>
+    <title>大佬用PHPStorm写的php程序</title>
+</head>
+<body>
+    <h1>大佬首发</h1>
+    <?php
+    echo "大佬的第一份php<br>";
+    echo "大佬要推到github<br>";
+    echo "小弟热烈欢迎大佬加入<br>";
+    echo "大佬万岁！！！<br>";
+    echo "<br><h2>嗯嗯，本大佬带你飞。</h2><br>";
     echo "而且不会翻车";
-?>
+    ?>
+</body>
+</html>
+
